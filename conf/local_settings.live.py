41fd26ed00447813cbc7027571802e63a627a7be
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2012-03-19 23:07:04 -0700

    setup of live database reference sqlite3 for now

--- conf/local_settings.live.py
+++ conf/local_settings.live.py
@@ -11,6 +11,17 @@ ADMINS = (
 
 SITE_ID = 2 # as per c9_sites.json fixtures
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_DIR, '../data/dev.db'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
 # setup for webfaction paths
 MEDIA_ROOT = os.path.join(PROJECT_DIR, '../../media/people')
 STATIC_ROOT = os.path.join(PROJECT_DIR, '../../static/people')
