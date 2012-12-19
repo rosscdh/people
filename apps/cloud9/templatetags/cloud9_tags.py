a3546977b3e6e300475fad240b36fdc43878c49b
Ross Crawford <sendrossemail@gmail.com>
2012-12-19 07:52:02 +0100

    Working Filters

--- apps/cloud9/templatetags/cloud9_tags.py
+++ apps/cloud9/templatetags/cloud9_tags.py
@@ -30,3 +30,44 @@ def people_search_form(context):
         'MEDIA_URL': context['MEDIA_URL'],
     })
 people_search_form.is_safe = True
+
+
+"""
+jQuery templates use constructs like:
+
+    {{if condition}} print something{{/if}}
+
+This, of course, completely screws up Django templates,
+because Django thinks {{ and }} mean something.
+
+Wrap {% verbatim %} and {% endverbatim %} around those
+blocks of jQuery templates and this will try its best
+to output the contents with no changes.
+"""
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+    
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
