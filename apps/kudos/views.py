ff1588ae9ddadefd63015b3d6be3fd87e8a078a8
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2013-02-14 08:56:48 -0800

    trying to get view working

--- apps/kudos/views.py
+++ apps/kudos/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views.generic import CreateView
+from mixins import JSONResponseMixin
+
+
+class AwardKudosView(JSONResponseMixin, CreateView):
+	def post(self, request, *args, **kwargs):
+		assert False
+
+
+class UndoAwardKudosView(JSONResponseMixin, CreateView):
+	def post(self, request, *args, **kwargs):
+		assert False
--- urls.py
+++ urls.py
@@ -9,8 +9,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_public_api.urls)),
-    url(r'^s2i/', include('django_select2.urls')),
 
+    url(r'^kudos/', include('apps.kudos.urls', namespace='kudos')),
     url(r'^social/', include('socialregistration.urls', namespace='socialregistration')),
     url(r'^org/', include('apps.orgchart.urls', namespace='orgchart')),
     url(r'^', include('apps.cloud9.urls', namespace='cloud9')),
